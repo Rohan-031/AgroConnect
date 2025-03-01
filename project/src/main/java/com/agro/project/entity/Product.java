c8233944acedf68ba0b552d9a9317d5555a49979
kshitij-narkhede <kshitij.22110941@viit.ac.in>
2025-03-02 00:30:45 +0530

    Product backend

--- project/src/main/java/com/agro/project/entity/Product.java
+++ project/src/main/java/com/agro/project/entity/Product.java
@@ -1,14 +1,16 @@
 package com.agro.project.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Data
+
 @Entity
 @Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
     @Column(name = "name")
     private String name;
@@ -23,9 +25,74 @@ public class Product {
     @Column(name = "selling_description")
     private String sellingDescription;
 
-    @ManyToOne
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "seller_id")
+    @JsonIgnore
     private Seller seller;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSellingDescription() {
+        return sellingDescription;
+    }
+
+    public void setSellingDescription(String sellingDescription) {
+        this.sellingDescription = sellingDescription;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
 
     public Product(String name, double price, int quantity, String category, String sellingDescription, String description) {
         this.name = name;
@@ -34,15 +101,6 @@ public class Product {
         this.category = category;
         this.sellingDescription = sellingDescription;
         this.description = description;
-    }
-
-    public Product(String name, double price, String category, int quantity, String description, String sellingDescription) {
-        this.name = name;
-        this.price = price;
-        this.category = category;
-        this.quantity = quantity;
-        this.description = description;
-        this.sellingDescription = sellingDescription;
     }
 
     public Product() {  }
--- project/src/main/java/com/agro/project/entity/Seller.java
+++ project/src/main/java/com/agro/project/entity/Seller.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+
 @Entity
 @Table(name = "Seller")
 public class Seller {
@@ -40,10 +40,17 @@ public class Seller {
     @Column(name = "longitude")
     private double longitude;
 
-    @OneToMany(mappedBy = "seller", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "seller", cascade ={CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.REFRESH})
     List<Product> productList;
 
     public Seller() {}
+
+    public Seller(Long chatId, String name, String email, String mobile) {
+        this.chatId = chatId;
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+    }
 
     public Seller(String name, String email, String password, String mobile, String address, String city, String state, double latitude, double longitude) {
         this.name = name;
@@ -65,11 +72,106 @@ public class Seller {
     }
 
     public void addProduct(Product product) {
-        if (this.productList == null) {
-            this.productList = new ArrayList<>();
+        if (productList == null) {
+            productList = new ArrayList<>();
         }
-//        productList.add(product);
-        this.productList.add(product);
+        productList.add(product);
+        product.setSeller(this);
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 }
